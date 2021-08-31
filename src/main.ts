8eef8f30e64c56c81c4b567d7204155c372d2afe
Danilo Barberini <danilo.barberini@fcamara.com.br>
2021-08-31 09:37:47 -0300

    fix(master): dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Acid Wallet API')
+    .setDescription('Digital wallet and payments API ;) ')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
